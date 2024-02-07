72573fc98cafb232319227d3552037357d89a78e
Ricardo <ricardo.remond.pro@gmail.com>
2024-02-07 09:43:03 +0100

    pause de 10h

--- controllers/controller-signup.php
+++ controllers/controller-signup.php
@@ -7,8 +7,10 @@ require_once "../models/Entreprise.php";
 $showform = true;
 $regex = "/^[a-zA-ZÀ-ÿ\_\-\d]+$/";
 
-Entreprise::Modifier_infos_entreprise("15789615151515", "caeen", "KingsMan", "1234567890", "nnatiV@gmail.com", "2 rrue de la briqueterie", "76500", "21");
+if ($_SERVER['REQUEST_METHOD'] == $_POST) {
+    $error = [];
 
+}
 
 ?>
 
--- views/view-signup.php
+++ views/view-signup.php
@@ -4,20 +4,150 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style/style.css">
     <title>Signin</title>
 </head>
 
-<body>
-    <h2>Page de connexion </h2>
+<body class="signin">
+    <h2>Page d'inscription</h2>
     <?php if ($showform == true) { ?>
         <div class="form_inscription">
             <form action="../controllers/controller-signup.php" method="POST" novalidate>
 
                 <label for="nom_entreprise">Nom de l'entreprise</label>
 
-                <input type="text" id="nom_entreprise" name="nom_entreprise"
-                    value="<?= isset($_POST['nom_entreprise']) ? htmlspecialchars($_POST['nom_entreprise']) : '' ?>">
+                <input type="text" id="nom_entreprise" name="nom_entreprise" value="
+                <?=
+                    isset($_POST['nom_entreprise']) ? htmlspecialchars($_POST['nom_entreprise']) : ''
+                    ?>">
+
+                <span class="error">
+                    <?php
+                    if (isset($error['nom_entreprise'])) {
+                        echo $error['nom_entreprise'];
+                    }
+                    ?>
+                </span>
+
+                <label for="ville">Ville</label>
+
+                <input type="text" id="ville" name="ville" value="
+                <?=
+                    isset($_POST['ville']) ? htmlspecialchars($_POST['ville']) : ''
+                    ?>">
+
+                <span class="error">
+                    <?php
+                    if (isset($error['ville'])) {
+                        echo $error['ville'];
+                    }
+                    ?>
+                </span>
+
+                <label for="email_entreprise">Email</label>
+
+                <input type="email" id="email_entreprise" name="email_entreprise" value="
+                <?=
+                    isset($_POST['email_entreprise']) ? htmlspecialchars($_POST['email_entreprise']) : ''
+                    ?>">
+
+                <span class="error">
+                    <?php
+                    if (isset($error['email_entreprise'])) {
+                        echo $error['email_entreprise'];
+                    }
+                    ?>
+                </span>
+
+                <label for="mdp_email_entreprise">Mot de passe</label>
+
+                <input type="password" id="mdp_email_entreprise" name="mdp_email_entreprise" value="<?=
+                    isset($_POST['mdp_email_entreprise']) ? ($_POST['mdp_email_entreprise']) : ''
+                    ?>">
+
+                <span class="error">
+                    <?php
+                    if (isset($error['mdp_email_entreprise'])) {
+                        echo $error['mdp_email_entreprise'];
+                    }
+                    ?>
+                </span>
+
+                <label for="verif_mdp_email_entreprise">Confimer le mot de passe </label>
+
+                <input type="password" id="verif_mdp_email_entreprise" name="verif_mdp_email_entreprise" value="<?=
+                    isset($_POST['verif_mdp_email_entreprise']) ? ($_POST['verif_mdp_email_entreprise']) : ''
+                    ?>">
+
+                <span class="error">
+                    <?php
+                    if (isset($error['verif_mdp_email_entreprise'])) {
+                        echo $error['verif_mdp_email_entreprise'];
+                    }
+                    ?>
+                </span>
+
+                <label for="numero_de_siret">Numéro de siret</label>
+
+                <input type="text" id="numero_de_siret" name="numero_de_siret" value="
+                <?=
+                    isset($_POST['numero_de_siret']) ? htmlspecialchars($_POST['numero_de_siret']) : ''
+                    ?>">
+
+                <span class="error">
+                    <?php
+                    if (isset($error['numero_de_siret'])) {
+                        echo $error['numero_de_siret'];
+                    }
+                    ?>
+                </span>
+
+                <label for="adresse_entreprise">Adresse postale</label>
+
+                <input type="text" id="adresse_entreprise" name="adresse_entreprise" value="
+                <?=
+                    isset($_POST['adresse_entreprise']) ? htmlspecialchars($_POST['adresse_entreprise']) : ''
+                    ?>">
+
+                <span class="error">
+                    <?php
+                    if (isset($error['adresse_entreprise'])) {
+                        echo $error['adresse_entreprise'];
+                    }
+                    ?>
+                </span>
+
+                <label for="code_postal_entreprise">Code postale</label>
+
+                <input type="text" id="code_postal_entreprise" name="code_postal_entreprise" value="
+                <?=
+                    isset($_POST['code_postal_entreprise']) ? htmlspecialchars($_POST['code_postal_entreprise']) : ''
+                    ?>">
+
+                <span class="error">
+                    <?php
+                    if (isset($error['code_postal_entreprise'])) {
+                        echo $error['code_postal_entreprise'];
+                    }
+                    ?>
+                </span>
+
+                <label for="cgu">
+                    <a href="#">
+                        Conditions général d'utilisation
+                    </a>
+                    <input class="cache" name="cgu" type="checkbox" value="Accepter les CGU ?" required>
+                </label>
+                <span class="error">
+                    <?php if (isset($errors['cgu'])) {
+                        echo $errors['cgu'];
+                    } ?>
+                </span>
+
+                <input name="enregister" id="enregister" type="submit" value="S'enregistrer">
+
             </form>
+
         </div>
 
 
