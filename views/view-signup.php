c950dda280d50374d3e05d0cd1c77f38984b791f
Ricardo <ricardo.remond.pro@gmail.com>
2024-02-09 13:47:12 +0100

    c'est trop nul le css avec materialize :/ je deprime :'(

--- views/view-signup.php
+++ views/view-signup.php
@@ -5,125 +5,164 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../node_modules/materialize-css/dist/css/materialize.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="../assets/style/style.css">
     <title>Signin</title>
 </head>
 
-<body class="signin">
+<body class="signin green darken-3">
 
-    <h2>Page d'inscription</h2>
+    <div class="container">
+        <h2>
+            <i class="medium material-icons left">account_box</i>
+            Page d'inscription
+        </h2>
+    </div>
+
 
     <?php if ($showform == true) { ?>
-        <div class="form_inscription">
-            <form action="../controllers/controller-signup.php" method="POST" novalidate>
+        <div class="container indigo lighten-3">
+            <form class="col s12" action="../controllers/controller-signup.php" method="POST" novalidate>
 
-                <label for="nom_entreprise">Nom de l'entreprise</label>
+                <div class="row">
 
-                <input type="text" id="nom_entreprise" name="nom_entreprise" value="<?=
-                    isset($_POST['nom_entreprise']) ? htmlspecialchars($_POST['nom_entreprise']) : '' ?>">
+                    <div class="input-field col s6">
+                        <label for="nom_entreprise"></label>
+                        <input placeholder="Nom de l'entreprise" type="text" id="nom_entreprise" name="nom_entreprise"
+                            class="validate" value="<?=
+                                isset($_POST['nom_entreprise']) ? htmlspecialchars($_POST['nom_entreprise']) : '' ?>">
 
-                <span class="error">
-                    <?php
-                    if (isset($error['nom_entreprise'])) {
-                        echo $error['nom_entreprise'];
-                    }
-                    ?>
-                </span>
+                        <span class="error">
+                            <?php
+                            if (isset($error['nom_entreprise'])) {
+                                echo $error['nom_entreprise'];
+                            }
+                            ?>
+                        </span>
+                    </div>
 
-                <label for="ville">Ville</label>
+                    <div class="input-field col s6">
+                        <label for="ville"></label>
+                        <input placeholder="Ville" type="text" id="ville" name="ville" value="<?=
+                            isset($_POST['ville']) ? htmlspecialchars($_POST['ville']) : '' ?>">
 
-                <input type="text" id="ville" name="ville" value="<?=
-                    isset($_POST['ville']) ? htmlspecialchars($_POST['ville']) : '' ?>">
+                        <span class="error">
+                            <?php
+                            if (isset($error['ville'])) {
+                                echo $error['ville'];
+                            }
+                            ?>
+                        </span>
 
-                <span class="error">
-                    <?php
-                    if (isset($error['ville'])) {
-                        echo $error['ville'];
-                    }
-                    ?>
-                </span>
+                    </div>
 
-                <label for="email_entreprise">Email</label>
+                </div>
 
-                <input type="email" id="email_entreprise" name="email_entreprise" value="<?=
-                    isset($_POST['email_entreprise']) ? htmlspecialchars($_POST['email_entreprise']) : '' ?>">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <label for="email_entreprise"></label>
+                        <input placeholder="Email" type="email" id="email_entreprise" name="email_entreprise" value="<?=
+                            isset($_POST['email_entreprise']) ? htmlspecialchars($_POST['email_entreprise']) : '' ?>">
 
-                <span class="error">
-                    <?php
-                    if (isset($error['email_entreprise'])) {
-                        echo $error['email_entreprise'];
-                    }
-                    ?>
-                </span>
+                        <span class="error">
+                            <?php
+                            if (isset($error['email_entreprise'])) {
+                                echo $error['email_entreprise'];
+                            }
+                            ?>
+                        </span>
+                    </div>
+                </div>
 
-                <label for="mdp_email_entreprise">Mot de passe</label>
+                <div class="row">
+                    <div class="input-field col s6">
+                        <label for="mdp_email_entreprise"></label>
+                        <input placeholder="Mot de passe" type="password" id="mdp_email_entreprise"
+                            name="mdp_email_entreprise"
+                            value="<?=
+                                isset($_POST['mdp_email_entreprise']) ? htmlspecialchars($_POST['mdp_email_entreprise']) : '' ?>">
 
-                <input type="password" id="mdp_email_entreprise" name="mdp_email_entreprise" value="<?=
-                    isset($_POST['mdp_email_entreprise']) ? htmlspecialchars($_POST['mdp_email_entreprise']) : '' ?>">
+                        <span class="error">
+                            <?php
+                            if (isset($error['mdp_email_entreprise'])) {
+                                echo $error['mdp_email_entreprise'];
+                            }
+                            ?>
+                        </span>
+                    </div>
 
-                <span class="error">
-                    <?php
-                    if (isset($error['mdp_email_entreprise'])) {
-                        echo $error['mdp_email_entreprise'];
-                    }
-                    ?>
-                </span>
+                    <div class="input-field col s6">
+                        <label for="verif_mdp"></label>
+                        <input placeholder="Confirmer le mot de passe " type="password" id="verif_mdp" name="verif_mdp"
+                            value="<?=
+                                isset($_POST['verif_mdp']) ? htmlspecialchars($_POST['verif_mdp']) : '' ?>">
 
-                <label for="verif_mdp">Confirmer le mot de passe </label>
+                        <span class="error">
+                            <?php
+                            if (isset($error['verif_mdp'])) {
+                                echo $error['verif_mdp'];
+                            }
+                            ?>
+                        </span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <label for="numero_de_siret"></label>
+                        <input placeholder="Numéro de siret" type="text" id="numero_de_siret" name="numero_de_siret" value="<?=
+                            isset($_POST['numero_de_siret']) ? htmlspecialchars($_POST['numero_de_siret']) : '' ?>">
 
-                <input type="password" id="verif_mdp" name="verif_mdp" value="<?=
-                    isset($_POST['verif_mdp']) ? htmlspecialchars($_POST['verif_mdp']) : '' ?>">
+                        <span class="error">
+                            <?php
+                            if (isset($error['numero_de_siret'])) {
+                                echo $error['numero_de_siret'];
+                            }
+                            ?>
+                        </span>
+                    </div>
+                </div>
 
-                <span class="error">
-                    <?php
-                    if (isset($error['verif_mdp'])) {
-                        echo $error['verif_mdp'];
-                    }
-                    ?>
-                </span>
+                <div class="row">
+                    <div class="input-field col s6">
+                        <label for="adresse_entreprise"></label>
+                        <input placeholder="Adresse postale" type="text" id="adresse_entreprise" name="adresse_entreprise"
+                            value="<?=
+                                isset($_POST['adresse_entreprise']) ? htmlspecialchars($_POST['adresse_entreprise']) : '' ?>">
 
-                <label for="numero_de_siret">Numéro de siret</label>
+                        <span class="error">
+                            <?php
+                            if (isset($error['adresse_entreprise'])) {
+                                echo $error['adresse_entreprise'];
+                            }
+                            ?>
+                        </span>
+                    </div>
 
-                <input type="text" id="numero_de_siret" name="numero_de_siret" value="<?=
-                    isset($_POST['numero_de_siret']) ? htmlspecialchars($_POST['numero_de_siret']) : '' ?>">
+                    <div class="input-field col s6">
+                        <label for="code_postal_entreprise"></label>
+                        <input placeholder="Code postale" type="text" id="code_postal_entreprise"
+                            name="code_postal_entreprise"
+                            value="<?=
+                                isset($_POST['code_postal_entreprise']) ? htmlspecialchars($_POST['code_postal_entreprise']) : '' ?>">
 
-                <span class="error">
-                    <?php
-                    if (isset($error['numero_de_siret'])) {
-                        echo $error['numero_de_siret'];
-                    }
-                    ?>
-                </span>
-
-                <label for="adresse_entreprise">Adresse postale</label>
-
-                <input type="text" id="adresse_entreprise" name="adresse_entreprise" value="<?=
-                    isset($_POST['adresse_entreprise']) ? htmlspecialchars($_POST['adresse_entreprise']) : '' ?>">
-
-                <span class="error">
-                    <?php
-                    if (isset($error['adresse_entreprise'])) {
-                        echo $error['adresse_entreprise'];
-                    }
-                    ?>
-                </span>
-
-                <label for="code_postal_entreprise">Code postale</label>
-
-                <input type="text" id="code_postal_entreprise" name="code_postal_entreprise"
-                    value="<?=
-                        isset($_POST['code_postal_entreprise']) ? htmlspecialchars($_POST['code_postal_entreprise']) : '' ?>">
-
-                <span class="error">
-                    <?php
-                    if (isset($error['code_postal_entreprise'])) {
-                        echo $error['code_postal_entreprise'];
-                    }
-                    ?>
-                </span>
+                        <span class="error">
+                            <?php
+                            if (isset($error['code_postal_entreprise'])) {
+                                echo $error['code_postal_entreprise'];
+                            }
+                            ?>
+                        </span>
+                    </div>
+                </div>
 
 
-                <input name="enregister" id="enregister" type="submit" value="S'enregistrer">
+                <div class="container">
+                    <button class="btn waves-effect waves-light" name="enregister" id="enregister" type="submit"
+                        value="S'enregistrer">Valider l'inscription
+                        <i class="medium material-icons right">send</i>
+                    </button>
+                </div>
+
 
             </form>
 
